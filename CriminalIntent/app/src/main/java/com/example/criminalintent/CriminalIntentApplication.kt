fe7a41f8c4aad825d8f1733690b87fd0568c6baf
Александр <91898691+Sasha-Kybik@users.noreply.github.com>
2022-12-27 14:35:26 +0400

    Update CriminalIntentApplication.kt
    Удаление комментариев.

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CriminalIntentApplication.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CriminalIntentApplication.kt
@@ -4,10 +4,8 @@ import android.app.Application
 
 class CriminalIntentApplication : Application() {
 
-    // Включить в раздел application файла AndroidManifest.xml -> android:name=".CriminalIntentApplication"
-    // с целью создания экземпляра CriminalIntentApplication при запуске приложения
     override fun onCreate() {
         super.onCreate()
-        CrimeRepository.initialize(this) // Обязательная инициализация CrimeRepository при запуске приложения
+        CrimeRepository.initialize(this)
     }
 }
